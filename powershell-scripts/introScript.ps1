081b71d6786a807e9da39b93d1e6b6bd881af247
Kristoffer Stobbe <krisstobbe@yahoo.com>
2019-03-27 16:50:57 -0500

    Updated Readme File

---
+++ powershell-scripts/introScript.ps1
@@ -0,0 +1,3 @@
+﻿Get-WmiObject -Class Win32_OperatingSystem –ComputerName localhost |
+Select-Object -Property CSName,@{n=”Last Booted”;
+e={[Management.ManagementDateTimeConverter]::ToDateTime($_.LastBootUpTime)}}
