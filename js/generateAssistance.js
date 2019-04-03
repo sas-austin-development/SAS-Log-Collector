371f02eb279c17af67830488caf253902703c13b
Kristoffer Stobbe <krisstobbe@yahoo.com>
2019-04-03 18:01:11 -0500

    Final Update 4/3
    Implemented storage of User Email and Tracking number. Will need for later JS functions

--- js/generateAssistance.js
+++ js/generateAssistance.js
@@ -7,18 +7,35 @@
 /* Using the Hidden Function, we can also un-hide the SDW log collection section until User generated */
 /* IMPORTANT: Not sure if Hidden Manipulation is available in IE or Opera */
 
-/* Adding DOM Reference for ID's and Assigning them to Variables */
+
+/* Adding DOM Reference for ID's and Assigning them to Variables - These are all under the Collect Information for Log Assistance Section */
 var button = document.getElementById("generateLogAssistance");
 var input = document.getElementById("testId");
 var ul = document.getElementById("itemList");
+var email = document.getElementById("inputEmail");
+var trackingNumber = document.getElementById("inputTrackingNumber");
 
 /* Setting the Header and Body Hidden when the Site loads*/
 document.getElementById("testsdwcheck").hidden = true;
 document.getElementById("logColectorAssistance").hidden = true;
 
-/* Length Function - Finding the Length of an Input and Returning it */
+/* Length Function(s) - Finding the Length of an Input and Returning it */ /* Future Efficiency Increase- Implement this into an Array */
 function inputLength() {
 	return input.value.length;
+}
+function inputLengthEmail() {
+	return email.value.length;
+}
+function inputLengthTrackingNumber() {
+	return trackingNumber.value.length;
+}
+
+/* Output Email and Tracking Number Values for Storage Purposes*/
+function outputEmail() {
+	return email.value;
+}
+function outputTrackingNumber() {
+	return trackingNumber.value;
 }
 
 /* Create List Function - When called, it creates a new LI elements and appends it to the UL parent element */
@@ -30,27 +47,40 @@ function createListElement(){
 		console.log("List Element Added");
 }
 
-/* Add List Function after Click- If the User has Something in the Input Box, it will call the Create List Element Function */
+/* Add List Function after Click- If the User has Something in the 3 Input Boxes, it will call the Create List Element Function */
 function addListAfterClick(){
-	if (inputLength() > 0) {
+	if (inputLength() > 0 && inputLengthEmail() > 0 && inputLengthTrackingNumber() > 0 ) {
 		createListElement();
 	}
 }
 
 /* Add List After Keypress - Calls the createList Element Function if the Input is > 0  */
 function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keycode === 13) {
+	if (inputLength() > 0 && event.keycode === 13 /*Keycode 13 is the RETURN or ENTER key*/) { 
 		createListElement();
 	}
 }
 
 /* Adds Event Listeners for the Button and Input, when events happen, it calls the Listed Functions*/
 button.addEventListener("click", addListAfterClick);
-input.addEventListener("keypress", addListAfterKeypress);
+input.addEventListener("keypress", addListAfterKeypress); /*Listens Every Keypress and sends to addListAfterKeypress Function*/
 
 
-/* Appears the SDW Steps when the "Generate Log Assistance" is Pressed */
+/* Appears the SDW Steps when the "Generate Log Assistance" is Pressed if there is something in the Email && Tracking Number Field */
 button.addEventListener("click", function() {
-			  document.getElementById("testsdwcheck").hidden = false;
-			  document.getElementById("logColectorAssistance").hidden = false;
+	if (inputLengthEmail() > 0 && inputLengthTrackingNumber() > 0 /*Tracking Numbers are 10 Digits*/) 
+		{	
+			/* Next Steps Appear */
+			document.getElementById("testsdwcheck").hidden = false;
+			document.getElementById("logColectorAssistance").hidden = false;
+
+			/* Hold the User's Email & Tracking Number */
+			var emailHold = outputEmail();
+			var trackingNumberHold = outputTrackingNumber();
+			console.log("User's Email: " + emailHold);
+			console.log("User's Tracking Number: " + trackingNumberHold);
+		}	  
+	else{
+		alert("Please enter a Valid Email and Tracking Number"); 
+	}
 }, false);
